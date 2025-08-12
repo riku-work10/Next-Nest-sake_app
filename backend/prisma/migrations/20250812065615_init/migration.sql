ff2e198eb4d009dcce7fc0358e0fb1dfe36f0b7b
riku-work10 <ukir19990530@gmail.com>
2025-08-12 15:59:12 +0900

    エピソードテーブル作成

---
+++ backend/prisma/migrations/20250812065615_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `episodes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `episodes` ADD CONSTRAINT `episodes_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
