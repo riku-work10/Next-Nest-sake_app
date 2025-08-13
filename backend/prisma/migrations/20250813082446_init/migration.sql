d476438f72fe01d40d088e8ed9216897997a2378
riku-work10 <ukir19990530@gmail.com>
2025-08-13 17:28:02 +0900

    マイグレーション作り直し、auth関係作成

--- backend/prisma/migrations/20250813082446_init/migration.sql
+++ backend/prisma/migrations/20250813082446_init/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `episodes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EpisodeModule } from './episode/episode.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EpisodeModule, UserModule],
+  imports: [EpisodeModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
